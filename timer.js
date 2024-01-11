cf4b0839baf1cd0b3ea4e5aa2068e7a9f12864d7
zoeleca <zoennts@gmail.com>
2024-01-11 11:09:44 +0100

    bouton addevent, css termine, box html

--- timer.js
+++ timer.js
@@ -1,5 +1,7 @@
 //recuperer le timer 
 const timer = document.getElementById("timer");
+const timerPage = document.getElementById("timerPage");
+const timeChoice =  document.getElementById("timeChoice")
 
 //création de variables pour stocker les minutes (en secondes)
 let timeSeconds = 600; 
@@ -9,24 +11,37 @@ let breakSeconds = 600;
 let intervalX; 
 
 //fonction qui lance le timer
+const startBtn = document.getElementById("start")
+
 const start = () => {
     timeSeconds = parseInt(document.getElementById("timeS").innerHTML)*60;
     breakSeconds = parseInt(document.getElementById("timeB").innerHTML)*60;
 //toute les secondes, lance la fonction time et change l'interval
-    intervalX = setInterval(screenTime, 1000)
+    intervalX = setInterval(screenTime, 1000); 
+
+    timerPage.style.display = 'flex';
+    timeChoice.style.display = "none";
 }
+startBtn.addEventListener("click", start)
+
 
 //function qui arrête le timer
+const stopBtn = document.getElementById("stop")
 const stop = () => {
     clearInterval(intervalX);
     console.log("stop")
 }
+stopBtn.addEventListener("click", stop)
 
 //fonction qui remet à 0 
+const resetBtn = document.getElementById("reset")
 const reset = () => {
     clearInterval(intervalX);
     timer.innerText = "00:00"
+    timeChoice.style.display = 'flex';
+    timerPage.style.display = "none";
 }
+resetBtn.addEventListener("click", reset)
 
 // countdown écran 
 const screenTime = () => {
