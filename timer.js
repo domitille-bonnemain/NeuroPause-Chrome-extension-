4d1932f2ad79684b14a39797ccf832a07b880f36
domitille-bonnemain <domi.roupy@gmail.com>
2024-01-11 15:14:03 +0100

    Luminosité revient à la normal à la fin du timer

--- timer.js
+++ timer.js
@@ -1,81 +1,91 @@
+// Fonction pour ajuster la luminosité au début du timer
 function adjustBrightnessAtTimerStart() {
-    // Définir la luminosité à 0.8
-    document.body.style.filter = "brightness(0.5)";
+    // Définir la luminosité à 0.4
+    document.body.style.filter = "brightness(0.4)";
 }
 
-//recuperer le timer 
+// Fonction pour réinitialiser la luminosité à sa valeur normale
+function resetBrightness() {
+    // Réinitialiser la luminosité à la normale (1)
+    document.body.style.filter = "brightness(1)";
+}
+
+// Récupérer le timer
 const timer = document.getElementById("timer");
 const timerPage = document.getElementById("timerPage");
-const timeChoice =  document.getElementById("timeChoice")
+const timeChoice = document.getElementById("timeChoice");
 
-//création de variables pour stocker les minutes (en secondes)
-let timeSeconds = 600; 
-let breakSeconds = 600; 
+// Création de variables pour stocker les minutes (en secondes)
+let timeSeconds = 600;
+let breakSeconds = 600;
 
-//création d'un Interval (qui sera modifiable)
-let intervalX; 
+// Création d'un intervalle (qui sera modifiable)
+let intervalX;
 
-//fonction qui lance le timer
-const startBtn = document.getElementById("start")
+// Fonction qui lance le timer
+const startBtn = document.getElementById("start");
 
 const start = () => {
+    // Appeler la fonction pour ajuster la luminosité au début du timer
+    adjustBrightnessAtTimerStart();
 
-//Aappeler la fonction pour ajuster la luminosité au début du timer
+    timeSeconds = parseInt(document.getElementById("timeS").innerHTML) * 60;
+    breakSeconds = parseInt(document.getElementById("timeB").innerHTML) * 60;
 
-timeSeconds = parseInt(document.getElementById("timeS").innerHTML)*60;
-breakSeconds = parseInt(document.getElementById("timeB").innerHTML)*60;
-/*
-//Déclencher l'evènement "timerStart" au bon moment 
-timerElement.dispatchEvent(new Event("timerStart"));
-*/
-//toute les secondes, lance la fonction time et change l'interval
-intervalX = setInterval(screenTime, 1000); 
+    // Déclencher l'événement "timerStart" au bon moment
+    // timerElement.dispatchEvent(new Event("timerStart"));
 
-timerPage.style.display = 'flex';
-timeChoice.style.display = "none";
-}
-startBtn.addEventListener("click", start)
-startBtn.addEventListener("click", adjustBrightnessAtTimerStart, 1000)
+    // Toutes les secondes, lance la fonction time et change l'interval
+    intervalX = setInterval(screenTime, 1000);
 
+    timerPage.style.display = 'flex';
+    timeChoice.style.display = "none";
+};
 
-//function qui arrête le timer
-const stopBtn = document.getElementById("stop")
+startBtn.addEventListener("click", start);
+
+// Fonction qui arrête le timer
+const stopBtn = document.getElementById("stop");
 const stop = () => {
     clearInterval(intervalX);
-    console.log("stop")
-}
-stopBtn.addEventListener("click", stop)
+    console.log("stop");
+};
 
-//fonction qui remet à 0 
-const resetBtn = document.getElementById("reset")
+stopBtn.addEventListener("click", stop);
+
+// Fonction qui remet à 0
+const resetBtn = document.getElementById("reset");
 const reset = () => {
     clearInterval(intervalX);
-    timer.innerText = "00:00"
+// Appeler la fonction pour réinitialiser la luminosité
+    resetBrightness(); 
+    timer.innerText = "00:00";
     timeChoice.style.display = 'flex';
     timerPage.style.display = "none";
-}
-resetBtn.addEventListener("click", reset)
+};
 
-// countdown écran 
+resetBtn.addEventListener("click", reset);
+
+// Countdown écran
 const screenTime = () => {
-    //countdown de secondes
+    // Countdown de secondes
     timeSeconds--;
 
-    //timer afficher
+    // Timer affiché
     timer.innerText = Math.floor((timeSeconds / 60)).toString() + ":" + (timeSeconds % 60).toString();
-    if(timeSeconds === 0){
+    if (timeSeconds === 0) {
         clearInterval(intervalX);
         intervalX = setInterval(breakCountdown, 1000);
     }
-}
+};
 
-//countdown break
+// Countdown break
 const breakCountdown = () => {
     breakSeconds--;
 
     timer.innerText = Math.floor((breakSeconds / 60)).toString() + ":" + (breakSeconds % 60).toString();
-    if(breakSeconds === 0){
+    if (breakSeconds === 0) {
         reset();
         alert("C'est reparti :)");
     }
-}
+};
