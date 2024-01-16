949392af3aa63f78b7e7b85ddc6f1d07791eda6a
domitille-bonnemain <domi.roupy@gmail.com>
2024-01-16 11:50:07 +0100

    commentaires ajoutés dans content et blur

--- blur.js
+++ blur.js
@@ -1,14 +1,22 @@
-
+// Écouteur d'événement pour les messages provenant de l'extension
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  // Vérifie si le basculement (toggle) est activé
   const isEnabled = message.toggleState;
+
+  // Si le basculement est activé
   if (isEnabled) {
+    // Récupère tous les onglets du navigateur
     chrome.tabs.query({}, function (tabs) {
+      // Gère les erreurs éventuelles
       if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError);
-        return; // Handle the error here
+        return; // Gère l'erreur ici
       }
 
+      // Affiche les informations sur les onglets dans la console
       console.log(tabs);
+
+      // Applique un filtre de luminosité à chaque onglet
       tabs.forEach(tab => {
         chrome.scripting.executeScript({
           target: { tabId: tab.id },
@@ -17,14 +25,17 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
           }
         });
       });
-    }); 
-  } else {
-    chrome.tabs.query({ }, function (tabs) {
+    });
+  } else { // Si le basculement est désactivé
+    // Récupère tous les onglets du navigateur
+    chrome.tabs.query({}, function (tabs) {
+      // Gère les erreurs éventuelles
       if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError);
-        return; // Handle the error here
+        return; // Gère l'erreur ici
       }
 
+      // Applique le filtre "none" pour réinitialiser la luminosité sur chaque onglet
       tabs.forEach(tab => {
         chrome.scripting.executeScript({
           target: { tabId: tab.id },
@@ -37,11 +48,11 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   }
 });
 
-
+// Écouteur d'événement pour la création d'un nouvel onglet
 chrome.tabs.onCreated.addListener(function (tab) {
+  // Affiche les informations sur le nouvel onglet dans la console
   console.log(tab);
 });
-
 
 
 
--- content.js
+++ content.js
@@ -1,9 +1,15 @@
+// Attend que la fenêtre soit entièrement chargée
 window.onload = function() {
-    chrome.storage.sync.get({ isEnabled: false }, function (data) {
-        let isEnabled = data.isEnabled;
-        chrome.runtime.sendMessage({ isEnabled: isEnabled});
-      });
+  // Utilise l'API de stockage synchronisé de Chrome pour récupérer des données
+  chrome.storage.sync.get({ isEnabled: false }, function (data) {
+      // Récupère la valeur stockée pour la clé 'isEnabled'
+      let isEnabled = data.isEnabled;
+
+      // Envoie un message à l'arrière-plan de l'extension avec la valeur de 'isEnabled'
+      chrome.runtime.sendMessage({ isEnabled: isEnabled });
+  });
 }
+
 
 
   
