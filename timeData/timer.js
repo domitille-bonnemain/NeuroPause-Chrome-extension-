363a9e89a60cfa71842ae8ca9926cc8b2021f2e4
domitille-bonnemain <domi.roupy@gmail.com>
2024-01-17 20:04:46 +0100

    commentaires pour chaque codes ajoutés

--- timeData/timer.js
+++ timeData/timer.js
@@ -1,8 +1,10 @@
+// Récupère l'élément d'affichage du temps dans la page HTML
 const timeScreen = document.getElementById("timeS");
 
-//sauvegarde des données
+// Quand le bouton "saveBtn" est cliqué, enregistre certaines informations dans un espace de stockage spécial du navigateur
 const saveBtn = document.getElementById("saveBtn");
 saveBtn.addEventListener("click", () => {
+  // Enregistre le temps à zéro, la valeur de l'élément d'affichage du temps, et indique que le chronomètre ne fonctionne pas (false)
   chrome.storage.local.set({
     timer: 0,
     timeScreen: timeScreen.value,
@@ -10,7 +12,9 @@ saveBtn.addEventListener("click", () => {
   });
 });
 
-//transformation des données en réponse (ds storage)
+// Lorsque la page est chargée, récupère la valeur enregistrée pour le temps d'écran depuis l'espace de stockage
 chrome.storage.local.get(["timeScreen"], (res) => {
-  timeScreen.value = res.timeScreen
+  // Met à jour l'affichage du temps avec la valeur récupérée
+  timeScreen.value = res.timeScreen;
 });
+
